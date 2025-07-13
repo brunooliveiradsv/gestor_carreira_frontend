750839a45760c5fe66a9e297cc2d4205dd9b4a5b
Bruno Oliveira <brunooliveiradsv@gmail.com>
2025-07-12 22:46:37 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -3,28 +3,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
-
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './contextos/AuthContext.jsx';
-
-// Imports do Tema
+import './index.css'; // Mantenha seus estilos globais se houver
 import { ThemeProvider } from '@mui/material/styles';
-import { tema } from './tema.js';
-
-import { NotificationProvider } from './contextos/NotificationContext.jsx'; // Importa o novo provedor
+import CssBaseline from '@mui/material/CssBaseline'; // Reseta CSS e aplica o tema base do MUI
+import darkTheme from './tema'; // Importe seu tema dark
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={tema}>
-      {/* Envolvemos o AuthProvider com o NotificationProvider */}
-      <NotificationProvider>
-        <AuthProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </AuthProvider>
-      </NotificationProvider>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline /> {/* Aplica estilos base do Material-UI e reinicia o CSS */}
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
 );
