6cce4db62637f255eb3102bbf26869e8af0526ca
Bruno Oliveira <brunooliveiradsv@gmail.com>
2025-07-12 22:47:56 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,20 @@
 // src/main.jsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // Mantenha seus estilos globais se houver
+import './index.css';
 import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline'; // Reseta CSS e aplica o tema base do MUI
-import darkTheme from './tema'; // Importe seu tema dark
+import CssBaseline from '@mui/material/CssBaseline';
+import darkTheme from './tema';
+import { BrowserRouter } from 'react-router-dom'; // <-- Importe BrowserRouter aqui
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
-      <CssBaseline /> {/* Aplica estilos base do Material-UI e reinicia o CSS */}
-      <App />
+      <CssBaseline />
+      <BrowserRouter> {/* <-- Envolva sua aplicação com BrowserRouter */}
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
 );
