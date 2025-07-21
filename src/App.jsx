248d0855c6d91344052c22e7f8761b5ea1033cfd
brunooliveiradsv <brunooliveiradsv@gmail.com>
2025-07-20 21:20:13 -0300

    Ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
+// src/App.jsx
 import { Routes, Route } from "react-router-dom";
 import Autenticacao from "./paginas/Autenticacao.jsx";
 import Dashboard from "./paginas/Dashboard.jsx";
-import Agenda from "./paginas/Agenda.jsx";
-import Financeiro from "./paginas/Financeiro.jsx";
-import Conquistas from "./paginas/Conquistas.jsx";
+// ... (outras importações de páginas)
+import Mural from "./paginas/Mural.jsx";
+import Assinatura from "./paginas/Assinatura.jsx";
+
 import RotaProtegida from "./componentes/RotaProtegida.jsx";
 import LayoutPrincipal from "./componentes/LayoutPrincipal.jsx";
-import Contatos from "./paginas/Contatos.jsx";
-import Configuracoes from "./paginas/Configuracoes.jsx";
-import Repertorio from "./paginas/Repertorio.jsx";
-import Equipamentos from "./paginas/Equipamentos.jsx";
-import RecuperarSenha from "./paginas/RecuperarSenha.jsx";
-import Setlists from "./paginas/Setlists.jsx";
-import EditorDeSetlist from "./paginas/EditorDeSetlist.jsx";
-import PaginaVitrine from "./paginas/PaginaVitrine.jsx"; 
-import AdminPainel from "./paginas/AdminPainel.jsx";
-import AdminUsuarios from "./paginas/AdminUsuarios.jsx";
-import AdminMusicas from "./paginas/AdminMusicas.jsx";
-import AdminSugestoes from "./paginas/AdminSugestoes.jsx";
-import AdminLogs from "./paginas/AdminLogs.jsx";
-import Mural from "./paginas/Mural.jsx";
-import Assinatura from "./paginas/Assinatura.jsx"; // <-- Importando a nova página de Assinatura
+// --- 1. IMPORTE O NOVO COMPONENTE ---
+import VerificarAssinatura from "./componentes/VerificarAssinatura.jsx";
 
 function App() {
   return (
@@ -29,9 +18,9 @@ function App() {
       <Route path="/login" element={<Autenticacao />} />
       <Route path="/cadastro" element={<Autenticacao />} />
       <Route path="/recuperar-senha" element={<RecuperarSenha />} />
-      <Route path="/vitrine/:url_unica" element={<PaginaVitrine />} /> {/* <-- ROTA PÚBLICA NOVA */}
+      <Route path="/vitrine/:url_unica" element={<PaginaVitrine />} />
 
-      {/* Rotas Protegidas */}
+      {/* Rotas Protegidas (exigem apenas login) */}
       <Route
         element={
           <RotaProtegida>
@@ -39,24 +28,32 @@ function App() {
           </RotaProtegida>
         }
       >
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/agenda" element={<Agenda />} />
-        <Route path="/financeiro" element={<Financeiro />} />
-        <Route path="/conquistas" element={<Conquistas />} />
-        <Route path="/contatos" element={<Contatos />} />
+        {/* --- 2. ROTAS QUE NÃO PRECISAM DE ASSINATURA ATIVA --- */}
+        {/* O usuário sempre pode aceder a estas páginas para gerir a sua conta */}
+        <Route path="/assinatura" element={<Assinatura />} />
         <Route path="/configuracoes" element={<Configuracoes />} />
-        <Route path="/repertorio" element={<Repertorio />} />
-        <Route path="/equipamentos" element={<Equipamentos />} />
-        <Route path="/setlists" element={<Setlists />} />
-        <Route path="/setlists/editar/:id" element={<EditorDeSetlist />} />
-        <Route path="/admin" element={<AdminPainel />} />
-        <Route path="/admin/usuarios" element={<AdminUsuarios />} />
-        <Route path="/admin/musicas" element={<AdminMusicas />} />
-        <Route path="/admin/sugestoes" element={<AdminSugestoes />} />
-        <Route path="/admin/logs" element={<AdminLogs />} />
-        <Route path="/mural" element={<Mural />} />
-        <Route path="/assinatura" element={<Assinatura />} /> {/* <-- ROTA PROTEGIDA NOVA */}
-        
+
+        {/* --- 3. ROTAS QUE EXIGEM ASSINATURA ATIVA --- */}
+        {/* O novo componente "abraça" todas as rotas que precisam de pagamento */}
+        <Route element={<VerificarAssinatura />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/agenda" element={<Agenda />} />
+          <Route path="/financeiro" element={<Financeiro />} />
+          <Route path="/conquistas" element={<Conquistas />} />
+          <Route path="/contatos" element={<Contatos />} />
+          <Route path="/repertorio" element={<Repertorio />} />
+          <Route path="/equipamentos" element={<Equipamentos />} />
+          <Route path="/setlists" element={<Setlists />} />
+          <Route path="/setlists/editar/:id" element={<EditorDeSetlist />} />
+          <Route path="/mural" element={<Mural />} />
+          
+          {/* As rotas de Admin também podem ser colocadas aqui dentro se exigirem assinatura */}
+          <Route path="/admin" element={<AdminPainel />} />
+          <Route path="/admin/usuarios" element={<AdminUsuarios />} />
+          <Route path="/admin/musicas" element={<AdminMusicas />} />
+          <Route path="/admin/sugestoes" element={<AdminSugestoes />} />
+          <Route path="/admin/logs" element={<AdminLogs />} />
+        </Route>
       </Route>
     </Routes>
   );
