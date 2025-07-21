12b9687735e12c7f57ad2b106aade2b2df2b8c46
brunooliveiradsv <brunooliveiradsv@gmail.com>
2025-07-20 21:23:14 -0300

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-// src/App.jsx
 import { Routes, Route } from "react-router-dom";
 import Autenticacao from "./paginas/Autenticacao.jsx";
 import Dashboard from "./paginas/Dashboard.jsx";
-// ... (outras importações de páginas)
-import Mural from "./paginas/Mural.jsx";
-import Assinatura from "./paginas/Assinatura.jsx";
-
+import Agenda from "./paginas/Agenda.jsx";
+import Financeiro from "./paginas/Financeiro.jsx";
+import Conquistas from "./paginas/Conquistas.jsx";
 import RotaProtegida from "./componentes/RotaProtegida.jsx";
 import LayoutPrincipal from "./componentes/LayoutPrincipal.jsx";
-// --- 1. IMPORTE O NOVO COMPONENTE ---
+import Contatos from "./paginas/Contatos.jsx";
+import Configuracoes from "./paginas/Configuracoes.jsx";
+import Repertorio from "./paginas/Repertorio.jsx";
+import Equipamentos from "./paginas/Equipamentos.jsx";
+import RecuperarSenha from "./paginas/RecuperarSenha.jsx";
+import Setlists from "./paginas/Setlists.jsx";
+import EditorDeSetlist from "./paginas/EditorDeSetlist.jsx";
+import PaginaVitrine from "./paginas/PaginaVitrine.jsx"; 
+import AdminPainel from "./paginas/AdminPainel.jsx";
+import AdminUsuarios from "./paginas/AdminUsuarios.jsx";
+import AdminMusicas from "./paginas/AdminMusicas.jsx";
+import AdminSugestoes from "./paginas/AdminSugestoes.jsx";
+import AdminLogs from "./paginas/AdminLogs.jsx";
+import Mural from "./paginas/Mural.jsx";
+import Assinatura from "./paginas/Assinatura.jsx";
 import VerificarAssinatura from "./componentes/VerificarAssinatura.jsx";
 
 function App() {
