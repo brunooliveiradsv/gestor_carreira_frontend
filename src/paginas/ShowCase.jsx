7f92a6e0a875888c179e23803b6e0a66be6a665d
Bruno Oliveira <brunooliveiradsv@gmail.com>
2025-07-26 22:07:39 -0300

    fix

--- src/paginas/ShowCase.jsx
+++ src/paginas/ShowCase.jsx
@@ -29,6 +29,7 @@ import LinkIcon from '@mui/icons-material/Link';
 import ThumbUp from '@mui/icons-material/ThumbUp';
 import ThumbDown from '@mui/icons-material/ThumbDown';
 import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
+import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
 
 import EnqueteShowcase from '../componentes/EnqueteShowcase';
 
@@ -62,7 +63,6 @@ const LoginParaFas = () => {
     );
 };
 
-// --- ALTERAÇÃO 2: ESTADO DOS "GOSTOS" É RECEBIDO POR PROPS ---
 const SetlistDialog = ({ open, onClose, setlist, musicasCurtidas, onLikeMusica }) => {
     const { fa } = useFanAuth();
     const { mostrarNotificacao } = useNotificacao();
@@ -72,7 +72,7 @@ const SetlistDialog = ({ open, onClose, setlist, musicasCurtidas, onLikeMusica }
             mostrarNotificacao('Faça login como fã para curtir músicas!', 'info');
             return;
         }
-        onLikeMusica(musicaId); // Chama a função do componente pai
+        onLikeMusica(musicaId);
     };
     
     return (
@@ -295,9 +295,7 @@ const ShowCaseContent = () => {
   }, [url_unica]);
 
   useEffect(() => {
-    if (url_unica) {
-        buscarDadosVitrine();
-    }
+    if (url_unica) buscarDadosVitrine();
   }, [url_unica, buscarDadosVitrine]);
 
   useEffect(() => {
